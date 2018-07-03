81010c191369e8c38758bfcb688b2abe0f5d804d
jiarui <jiaruixu@umich.edu>
2018-07-03 16:18:32 -0400

    modify train.sh

--- Deeplab_mapillary/train/train.sh
+++ Deeplab_mapillary/train/train.sh
@@ -5,7 +5,7 @@ python ../models/research/deeplab/train.py \
     --logtostderr \
     --initialize_last_layer=False \
     --last_layers_contain_logits_only=True \
-    --training_number_of_steps=50000 \
+    --training_number_of_steps=150000 \
     --train_split="train" \
     --model_variant="xception_65" \
     --atrous_rates=6 \
@@ -15,16 +15,16 @@ python ../models/research/deeplab/train.py \
     --decoder_output_stride=4 \
     --train_crop_size=500 \
     --train_crop_size=500 \
-    --train_batch_size=8 \
-    --num_clones=2 \
+    --train_batch_size=16 \
+    --num_clones=4 \
     --dataset="mapillary" \
     --save_summaries_secs=60 \
     --base_learning_rate=0.001 \
     --learning_rate_decay_step=200 \
     --weight_decay=0.000015 \
-    --fine_tune_batch_norm=false\
+    --fine_tune_batch_norm=True \
     --save_summaries_images=True \
-    --tf_initial_checkpoint="/mnt/fcav/self_training_segmentation/pretrained_model/trainlogs/model.ckpt-50000" \
+    --tf_initial_checkpoint="/mnt/fcav/self_training_segmentation/pretrained_model/xception_71/model.ckpt" \
     --train_logdir="/mnt/fcav/self_training_segmentation/trainlogs/" \
     --dataset_dir="/mnt/fcav/self_training_segmentation/images/tfrecord/" 2>&1 | tee logs/train_$now.txt &
     #--tf_initial_checkpoint="pretrained_model/deeplabv3_pascal_trainval/model.ckpt" \
